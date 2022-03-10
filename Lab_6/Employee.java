9351a83a3df1264fde9636b8aa9762c167bd0bf1
JanaRammall <99910722+JanaRammall@users.noreply.github.com>
2022-03-10 18:40:38 +0200

    Add files via upload

---
+++ Lab_6/Employee.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Employee {
+    // Initializing variables
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double salary;
+
+    public Employee() {
+        // Get user input
+        Scanner scan = new Scanner(System.in);
+        System.out.println("enter your first name ");
+        firstName= scan.nextInt();
+        System.out.println("enter your last name ");
+        lastName = scan.nextLine();
+        System.out.println("enter your age :);
+        age = scan.nextInt();
+        System.out.println("enter your salary ");
+        salary = scan.nextDouble();
+           
+        
+         
+        
+    }
+
+    public String toString() {
+        return "Employee information :" + firstName+
+" "+lastName +","+age+","+salary ;
+    }
+}
