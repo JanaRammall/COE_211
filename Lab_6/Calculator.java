8e4182c6943818d93ea686ea5460b40eecfa4b2c
JanaRammall <99910722+JanaRammall@users.noreply.github.com>
2022-03-14 17:15:27 +0200

    Update Calculator.java

--- Lab_6/Calculator.java
+++ Lab_6/Calculator.java
@@ -12,8 +12,31 @@ public class Calculator {
         /**
          * Continue code here to ask for the first number, the operator, and the second number, IN THAT ORDER
          */
+        System.out.println("enter the first number  ");
+        num1 = scanCalc.nextInt();
+        scanCalc.nextLine();
+        System.out.println("enter  the operator  ");
+        operator  = scanCalc.nextLine();
+        System.out.println("enter the second number ");
+        num2 = scanCalc.nextInt();
 
-        // Decide on the operation to perform 
+        switch(operator ){
+            case "+":  System.out.print("the answer is:  "+add(num1,num2));
+break;
+            case "-" :
+                System.out.println("the answer is: "+subtract(num1,num2));
+            break;
+
+            case "/" :
+                System.out.println("the answer is: "+divide(num1,num2));
+            break;
+            case  "*":
+                System.out.println("the answer is : "+multiply (num1,num2));
+            default :
+        }
+
+
+        // Decide on the operation to perform
         /**
          * Insert code to decide on which function to call, based on the operator the user chose
          * Hint: Switch statement
@@ -21,26 +44,18 @@ public class Calculator {
     }
 
     public String add(int a, int b) {
-        /**
-         * Enter logic
-         */
+        return ""+(a+b) ;
     }
 
     public String subtract(int a, int b) {
-        /**
-         * Enter logic
-         */
+        return  ""+ (a-b);
     }
 
     public String multiply(int a, int b) {
-        /**
-         * Enter logic
-         */
+        return ""+(a*b);
     }
 
     public String divide(int a, int b) {
-        /**
-         * Enter logic
-         */
+       return ""+((double)a/b);
     }
 }
