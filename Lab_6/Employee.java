eae83728dae39784218ca5af4de8690d16b8e4ba
JanaRammall <99910722+JanaRammall@users.noreply.github.com>
2022-03-14 17:14:24 +0200

    Update Employee.java

--- Lab_6/Employee.java
+++ Lab_6/Employee.java
@@ -11,21 +11,20 @@ public class Employee {
         // Get user input
         Scanner scan = new Scanner(System.in);
         System.out.println("enter your first name ");
-        firstName= scan.nextInt();
+        firstName = scan.nextLine();
         System.out.println("enter your last name ");
         lastName = scan.nextLine();
-        System.out.println("enter your age :);
-        age = scan.nextInt();
+        System.out.println("enter your age :");
+                age = scan.nextInt();
         System.out.println("enter your salary ");
         salary = scan.nextDouble();
-           
-        
-         
-        
+
+
+
+
     }
 
-    public String toString() {
+    public  String toString() {
         return "Employee information :" + firstName+
-" "+lastName +","+age+","+salary ;
-    }
-}
+                " "+lastName +","+age+","+salary ;
+    }}
